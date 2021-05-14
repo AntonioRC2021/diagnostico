fb312cd1409dcb3748e1de459106bbaff1f2e81f
Antonio <tonymike20@hotmail.com>
2021-05-13 19:06:10 -0600

    Solo eso alcance a hacer profe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeliculasComponent } from './pages/peliculas/peliculas.component';
+import { SeriesComponent } from './pages/series/series.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'peliculas', component:PeliculasComponent},
+  {path:'series', component:SeriesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeliculasComponent } from './pages/peliculas/peliculas.component';
+import { SeriesComponent } from './pages/series/series.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculasComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule,
